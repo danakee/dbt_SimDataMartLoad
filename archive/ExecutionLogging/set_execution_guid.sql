cce1d4314634fd0831e2ea90beee11f75894e674
Dana Kee <Dana.Kee@FlightSafety.com>
2024-10-09 09:41:17 -0500

    Cleanup old archived files

--- archive/ExecutionLogging/set_execution_guid.sql
+++
@@ -1,6 +0,0 @@
-{% macro set_execution_guid() %}
-    {% if execute %}
-        {% set start_result = log_execution_start() %}
-        {% do context.update({'execution_guid': start_result.execution_guid}) %}
-    {% endif %}
-{% endmacro %}
