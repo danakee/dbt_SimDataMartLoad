da0ef350cee74f34fc652fd6cca51570b45682dc
Dana Kee <Dana.Kee@FlightSafety.com>
2024-10-15 16:53:03 -0500

    dbt_project.yml file was throwing syntax error in the editor so I updated these two referenced macros to eliminate the syntax error at preview time.

--- macros/log_run_parameters_start.sql
+++ macros/log_run_parameters_start.sql
@@ -1,29 +1,33 @@
 {% macro log_run_parameters_start() %}
 
-    {% set run_started_at_str = run_started_at.strftime('%Y-%m-%d %H:%M:%S') %}
-    {% set command_line = env_var('DBT_COMMAND_LINE', '') %}
+    {% if execute %}
+        {% set run_started_at_str = run_started_at.strftime('%Y-%m-%d %H:%M:%S') %}
+        {% set command_line = env_var('DBT_COMMAND_LINE', '') %}
 
-    {% if command_line == '' %}
-        {{ exceptions.raise_compiler_error("DBT_COMMAND_LINE environment variable is not set. Please set this variable before running dbt.") }}
+        {% if command_line == '' %}
+            {{ log("WARNING: DBT_COMMAND_LINE environment variable is not set. This is expected during syntax checking, but ensure you're using the wrapper script for actual runs.", info=True) }}
+        {% else %}
+            {{ log("Debug - Command Line from env: " ~ command_line, info=True) }}
+            
+            -- Insert the command line that was used to start the dbt run into the DBTExecutionParameterLog table
+            {% call statement('insert_run_parameters_start') %}
+                INSERT INTO [SimulationsAnalyticsLogging].[dbo].[DBTExecutionParameterLog] (
+                    InvocationGUID, 
+                    ParameterName, 
+                    ParameterValue, 
+                    LogDateTime
+                )
+                VALUES 
+                (
+                    '{{ invocation_id }}',
+                    'COMMAND_LINE',
+                    '{{ command_line | replace("''", "''''") }}',
+                    SYSDATETIMEOFFSET()
+                );
+            {% endcall %}
+        {% endif %}
+    {% else %}
+        {{ log("Skipping run parameter logging during parse/compile phase", info=True) }}
     {% endif %}
-
-    {{ log("Debug - Command Line from env: " ~ command_line, info=True) }}
-    
-    -- Insert the command line that was used to start the dbt run into the DBTExecutionParameterLog table
-    {% call statement('insert_run_parameters_start') %}
-        INSERT INTO [SimulationsAnalyticsLogging].[dbo].[DBTExecutionParameterLog] (
-            InvocationGUID, 
-            ParameterName, 
-            ParameterValue, 
-            LogDateTime
-        )
-        VALUES 
-        (
-            '{{ invocation_id }}',
-            'COMMAND_LINE',
-            '{{ command_line | replace("''", "''''") }}',
-            SYSDATETIMEOFFSET()
-        );
-    {% endcall %}
 
 {% endmacro %}
